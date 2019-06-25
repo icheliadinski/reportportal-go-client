1d631addffbd6a5730da53130d68dcb85ba92ae2
Igor Cheliadinski <icheliadinski@gmail.com>
2019-06-25 16:51:20 -0400

    add new client

--- app/reportportal/client.go
+++ app/reportportal/client.go
@@ -3,6 +3,7 @@ package reportportal
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/pkg/errors"
 )
@@ -14,20 +15,29 @@ type Client struct {
 	Project  string
 }
 
+func NewClient(endpoint string, token string) (*Client, error) {
+	c := &Client{}
+	c.Endpoint = strings.TrimSuffix(endpoint, "/")
+	c.Token = token
+	return c, nil
+}
+
 func (c *Client) CheckConnect() error {
 	url := fmt.Sprintf("%s/user", c.Endpoint)
-	r, err := http.NewRequest(http.MethodGet, url, nil)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return errors.Wrapf(err, "can't create a new request for %s", url)
 	}
-	r.Header.Set("Authorization", "Bearer "+c.Token)
+
+	auth := fmt.Sprintf("Bearer %s", c.Token)
+	req.Header.Set("Authorization", auth)
 
 	client := http.Client{}
-
-	resp, err := client.Do(r)
+	resp, err := client.Do(req)
 	defer func() { _ = resp.Body.Close() }()
+
 	if err != nil {
-		return errors.Wrapf(err, "failed to execute GET request %s", r.URL)
+		return errors.Wrapf(err, "failed to execute GET request %s", req.URL)
 	}
 	if resp.StatusCode != http.StatusOK {
 		return errors.Errorf("failed with status %s", resp.Status)
