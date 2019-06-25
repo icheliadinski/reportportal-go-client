410a0d7edf3b98ed8e9c9ba255b51806ca3e9ac3
Igor Cheliadinski <icheliadinski@gmail.com>
2019-06-25 17:21:05 -0400

    add params reap

--- app/main.go
+++ app/main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/icheliadinski/reportportal-go-client/app/reportportal"
+	"github.com/jessevdk/go-flags"
 )
 
 func main() {
-	c, err := reportportal.NewClient("", "")
-	if err := c.CheckConnect(); err != nil {
-		fmt.Println(err)
+	var opts reportportal.Client
+	if _, err := flags.Parse(&opts); err != nil {
+		os.Exit(1)
 	}
+	c := reportportal.NewClient(opts.Endpoint, opts.Token, opts.Launch, opts.Project)
+	if err := c.CheckConnect(); err != nil {
+		panic(err)
+	}
+	fmt.Println("Success")
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/icheliadinski/reportportal-go-client
 go 1.12
 
 require (
+	github.com/jessevdk/go-flags v1.4.0
 	github.com/pkg/errors v0.8.1
 	github.com/stretchr/testify v1.3.0
 )
