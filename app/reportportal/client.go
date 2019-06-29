cf0ed621ed81490869b035a7d2b6e9f37fb9f0b5
Igor Cheliadinski <icheliadinski@gmail.com>
2019-06-29 00:07:23 -0400

    add launch info

--- app/reportportal/client.go
+++ app/reportportal/client.go
@@ -20,6 +20,12 @@ type Client struct {
 	Project  string `short:"p" long:"project" env:"PROJECT" description:"project name"`
 }
 
+// LaunchInfo defines launch object
+type LaunchInfo struct {
+	id     string
+	number int64
+}
+
 // NewClient defines function constructor for client
 func NewClient(endpoint string, token string, launch string, project string) *Client {
 	e := strings.TrimSuffix(endpoint, "/")
@@ -55,8 +61,8 @@ func (c *Client) CheckConnect() error {
 	return nil
 }
 
-// StartLaunch defines launch start
-func (c *Client) StartLaunch(name string, description string, tags []string, startTime time.Time) error {
+// StartLaunch runs launch
+func (c *Client) StartLaunch(name string, description string, tags []string, startTime time.Time) (string, error) {
 	url := fmt.Sprintf("%s/%s/launch", c.Endpoint, c.Project)
 	launch := struct {
 		Name        string   `json:"name"`
@@ -72,13 +78,13 @@ func (c *Client) StartLaunch(name string, description string, tags []string, sta
 
 	b, err := json.Marshal(&launch)
 	if err != nil {
-		return errors.Wrapf(err, "failed to marshal object, %v", launch)
+		return "", errors.Wrapf(err, "failed to marshal object, %v", launch)
 	}
 
 	r := bytes.NewReader(b)
 	req, err := http.NewRequest(http.MethodPost, url, r)
 	if err != nil {
-		return errors.Wrapf(err, "failed to create request for %s", url)
+		return "", errors.Wrapf(err, "failed to create request for %s", url)
 	}
 
 	auth := fmt.Sprintf("Bearer %s", c.Token)
@@ -93,10 +99,20 @@ func (c *Client) StartLaunch(name string, description string, tags []string, sta
 		}
 	}()
 	if err != nil {
-		return errors.Wrapf(err, "failed to execute POST request %s", req.URL)
+		return "", errors.Wrapf(err, "failed to execute POST request %s", req.URL)
 	}
 	if resp.StatusCode != http.StatusCreated {
-		return errors.Errorf("failed with status %s", resp.Status)
+		return "", errors.Errorf("failed with status %s", resp.Status)
 	}
-	return nil
+
+	var li LaunchInfo
+	if err := json.NewDecoder(resp.Body).Decode(&li); err != nil {
+		return "", errors.Wrapf(err, "failed to decode response from %s", req.URL)
+	}
+	return li.id, nil
+}
+
+// StopLaunch stops the exact launch
+func (c *Client) StopLaunch() {
+
 }
