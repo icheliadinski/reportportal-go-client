ba27c6f123d51efaf2c219612ea0f39d2d683779
Igor Cheliadinski <icheliadinski@gmail.com>
2019-06-30 00:30:49 -0400

    add todo launch split

---
+++ app/reportportal/launch.go
@@ -0,0 +1,25 @@
+package reportportal
+
+// Launch defines launch info
+type Launch struct {
+}
+
+// Start starts the launch
+func (l *Launch) Start() error {
+	return nil
+}
+
+// Stop stops the launch
+func (l *Launch) Stop() error {
+	return nil
+}
+
+// Finish finishes launch
+func (l *Launch) Finish() error {
+	return nil
+}
+
+// Update updates launch
+func (l *Launch) Update() error {
+	return nil
+}
