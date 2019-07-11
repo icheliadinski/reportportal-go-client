a82ee384247dac382fc5707e5a26f23cb80def6c
Igor Cheliadinski <icheliadinski@gmail.com>
2019-07-10 22:15:26 -0400

    refactor launch

--- app/rp/helpers.go
+++ app/rp/helpers.go
@@ -1,24 +1,9 @@
 package rp
 
 import (
-	"fmt"
-	"net/http"
 	"time"
 )
 
-func addContentTypeJSON(r *http.Request) {
-	r.Header.Set("Content-Type", "application.json")
-}
-
 func toTimestamp(t time.Time) int64 {
 	return t.Unix() * int64(time.Microsecond)
-}
-
-func doRequest(req *http.Request, token string) (*http.Response, error) {
-	client := http.Client{}
-	if token != "" {
-		auth := fmt.Sprintf("Bearer %s", token)
-		req.Header.Set("Authorization", auth)
-	}
-	return client.Do(req)
 }
