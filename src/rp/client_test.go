58737eb8ef11b3e2a6b9c84df8d1128856520bd7
Igor Cheliadinski <icheliadinski@gmail.com>
2019-07-13 17:56:13 -0400

    fix tests

--- src/rp/client_test.go
+++ src/rp/client_test.go
@@ -6,32 +6,28 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type params struct {
-	Endpoint string
-	Version  int
-}
-
 func TestNewClient(t *testing.T) {
 	c := NewClient("", "", "", 0)
 	assert.NotNil(t, c)
 
 	var endpoints = []struct {
-		client   *params
+		endpoint string
+		version  int
 		expected string
 	}{
-		{&params{"rp.epam.com", 0}, "https://rp.epam.com/api/v1"},
-		{&params{"rp.epam.com", -1}, "https://rp.epam.com/api/v1"},
-		{&params{"rp.epam.com", 1}, "https://rp.epam.com/api/v1"},
-		{&params{"rp.epam.com", 2}, "https://rp.epam.com/api/v2"},
-		{&params{"https://rp.epam.com", 1}, "https://rp.epam.com/api/v1"},
-		{&params{"https://rp.epam.com/", 1}, "https://rp.epam.com/api/v1"},
-		{&params{"rp.epam.com/api/v1", 1}, "https://rp.epam.com/api/v1"},
+		{"rp.epam.com", 0, "https://rp.epam.com/api/v1"},
+		{"rp.epam.com", -1, "https://rp.epam.com/api/v1"},
+		{"rp.epam.com", 1, "https://rp.epam.com/api/v1"},
+		{"rp.epam.com", 2, "https://rp.epam.com/api/v2"},
+		{"https://rp.epam.com", 1, "https://rp.epam.com/api/v1"},
+		{"https://rp.epam.com/", 1, "https://rp.epam.com/api/v1"},
+		{"rp.epam.com/api/v1", 1, "https://rp.epam.com/api/v1"},
 	}
 
 	for _, tt := range endpoints {
-		actual := NewClient(tt.client.Endpoint, "", "", tt.client.Version)
+		actual := NewClient(tt.endpoint, "", "", tt.version)
 		if actual.Endpoint != tt.expected {
-			t.Errorf(`NewClient(%s, "", "", %d): expected %s, actual %s`, tt.client.Endpoint, tt.client.Version, tt.expected, actual.Endpoint)
+			t.Errorf(`NewClient(%s, "", "", %d): expected %s, actual %s`, tt.endpoint, tt.version, tt.expected, actual.Endpoint)
 		}
 	}
 }
