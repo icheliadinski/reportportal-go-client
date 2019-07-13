f866633cc3e06e8f9ed2f3a5d05cb00506b4a990
Igor Cheliadinski <icheliadinski@gmail.com>
2019-07-13 17:49:57 -0400

    add tests for client

--- src/rp/client_test.go
+++ src/rp/client_test.go
@@ -6,9 +6,32 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewClient(t *testing.T) {
-	c := NewClient("rp.epam.com", "test_project", "1234")
+type params struct {
+	Endpoint string
+	Version  int
+}
 
+func TestNewClient(t *testing.T) {
+	c := NewClient("", "", "", 0)
 	assert.NotNil(t, c)
-	assert.Equal(t, "https://rp.epam.com/api/v1", c.Endpoint)
+
+	var endpoints = []struct {
+		client   *params
+		expected string
+	}{
+		{&params{"rp.epam.com", 0}, "https://rp.epam.com/api/v1"},
+		{&params{"rp.epam.com", -1}, "https://rp.epam.com/api/v1"},
+		{&params{"rp.epam.com", 1}, "https://rp.epam.com/api/v1"},
+		{&params{"rp.epam.com", 2}, "https://rp.epam.com/api/v2"},
+		{&params{"https://rp.epam.com", 1}, "https://rp.epam.com/api/v1"},
+		{&params{"https://rp.epam.com/", 1}, "https://rp.epam.com/api/v1"},
+		{&params{"rp.epam.com/api/v1", 1}, "https://rp.epam.com/api/v1"},
+	}
+
+	for _, tt := range endpoints {
+		actual := NewClient(tt.client.Endpoint, "", "", tt.client.Version)
+		if actual.Endpoint != tt.expected {
+			t.Errorf(`NewClient(%s, "", "", %d): expected %s, actual %s`, tt.client.Endpoint, tt.client.Version, tt.expected, actual.Endpoint)
+		}
+	}
 }
