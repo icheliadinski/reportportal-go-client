812de3375f632beee7bf1116774cd1ad1261ce37
Igor Cheliadinski <icheliadinski@gmail.com>
2019-07-13 17:12:25 -0400

    add client test

--- src/rp/client_test.go
+++ src/rp/client_test.go
@@ -1,7 +1,14 @@
 package rp
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestNewClient(t *testing.T) {
+	c := NewClient("rp.epam.com", "test_project", "1234")
 
+	assert.NotNil(t, c)
+	assert.Equal(t, "https://rp.epam.com/api/v1", c.Endpoint)
 }
