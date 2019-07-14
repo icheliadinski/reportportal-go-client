ced541d0ac20262748b88275c627c303c7bc3524
Igor Cheliadinski <icheliadinski@gmail.com>
2019-07-14 14:54:20 -0400

    add check for status code

--- rp/launch_test.go
+++ rp/launch_test.go
@@ -14,21 +14,53 @@ func TestNewLaunch(t *testing.T) {
 }
 
 func TestStartLaunch(t *testing.T) {
-	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, "/test_project/launch", r.URL.Path)
-		assert.Equal(t, "application/json", r.Header.Get("Content-Type"))
-		w.WriteHeader(http.StatusCreated)
-		resp := `{"id": "testid123"}`
-		w.Write([]byte(resp))
-	}))
-	c := &Client{
-		Endpoint: testServer.URL,
-		Project:  "test_project",
-		Token:    "1234",
-	}
-	l := NewLaunch(c, "test launch", "test description", "test mode", nil)
-	err := l.Start()
+	t.Run("Correctly created", func(t *testing.T) {
+		okResponse := `{"id": "testid"}`
+		h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, "application/json", r.Header.Get("Content-Type"))
+			assert.Equal(t, "/test_project/launch", r.URL.Path)
+			w.WriteHeader(http.StatusCreated)
+			w.Write([]byte(okResponse))
+		})
+		s := httptest.NewServer(h)
 
-	assert.Equal(t, "testid123", l.Id)
-	assert.NoError(t, err)
+		c := &Client{
+			Endpoint: s.URL,
+			Project:  "test_project",
+		}
+		l := &Launch{
+			client:      c,
+			Name:        "test launch",
+			Description: "test description",
+			Mode:        "test mode",
+			Tags:        nil,
+		}
+		err := l.Start()
+
+		assert.Equal(t, "testid", l.Id)
+		assert.NoError(t, err)
+	})
+
+	t.Run("Differ status code", func(t *testing.T) {
+		h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
+		s := httptest.NewServer(h)
+
+		c := &Client{
+			Endpoint: s.URL,
+			Project:  "test_project",
+		}
+		l := &Launch{
+			client:      c,
+			Name:        "test launch",
+			Description: "test description",
+			Mode:        "test mode",
+			Tags:        nil,
+		}
+		err := l.Start()
+
+		assert.Error(t, err)
+		assert.Equal(t, err.Error(), "failed with status 200 OK")
+	})
 }
