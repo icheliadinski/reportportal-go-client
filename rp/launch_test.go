c272be453a3086a8e48ea5ff1095314db14c53ef
Igor Cheliadinski <icheliadinski@gmail.com>
2019-07-18 21:23:36 -0400

    add test for finish

--- rp/launch_test.go
+++ rp/launch_test.go
@@ -93,3 +93,31 @@ func TestStopLaunch(t *testing.T) {
 
 	assert.NoError(t, err)
 }
+
+func TestFinishLaunch(t *testing.T) {
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.URL.Path, "/test_project/launch/id123/finish")
+		assert.Equal(t, "PUT", r.Method)
+
+		d, err := ioutil.ReadAll(r.Body)
+		assert.NoError(t, err)
+		assert.Contains(t, string(d), `"status":"test status"`)
+		assert.Equal(t, "application/json", r.Header.Get("Content-Type"))
+	})
+	s := httptest.NewServer(h)
+
+	c := &Client{
+		Endpoint: s.URL,
+		Project:  "test_project",
+	}
+	l := &Launch{
+		client: c,
+		Name:   "test launch",
+		Mode:   "test mode",
+		Tags:   nil,
+		Id:     "id123",
+	}
+	err := l.Finish("test status")
+
+	assert.NoError(t, err)
+}
