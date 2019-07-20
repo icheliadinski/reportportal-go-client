a9f53a742b7e660b453287cadcb804f6d687f205
Igor Cheliadinski <icheliadinski@gmail.com>
2019-07-19 20:05:09 -0400

    add test for start item

--- rp/test-item_test.go
+++ rp/test-item_test.go
@@ -1,6 +1,8 @@
 package rp
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,4 +12,25 @@ func TestNewTestItem(t *testing.T) {
 	l := &Launch{}
 	ti := NewTestItem(l, "", "", "", nil, nil)
 	assert.NotNil(t, ti)
+}
+
+func TestStartTestItem(t *testing.T) {
+	t.Run("Wrong status code", func(t *testing.T) {
+		h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusInternalServerError)
+		})
+		s := httptest.NewServer(h)
+
+		ti := &TestItem{
+			launch: &Launch{
+				Id: "id123",
+			},
+			client: &Client{
+				Endpoint: s.URL,
+				Project:  "test_project",
+			},
+		}
+		err := ti.Start()
+		assert.EqualError(t, err, "failed with status 500 Internal Server Error")
+	})
 }
